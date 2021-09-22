c4a84ebcbdd80cd091a89fdeae170dec27d090f8
Shagun Madhikarmi <madhikarma@gmail.com>
2021-09-22 06:41:33 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -65,7 +65,7 @@ echo $(date)
 xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 8,OS=14.5" clean build-for-testing | xcpretty
 echo $(date)
 
-echo "Running tests.."
-echo $(date)
-xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 8,OS=14.5" test-without-building | xcpretty
-echo $(date)
+# echo "Running tests.."
+# echo $(date)
+# xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 8,OS=14.5" test-without-building | xcpretty
+# echo $(date)
