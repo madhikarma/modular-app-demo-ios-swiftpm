da8ad4efdf59d5e658a6b51f10ff0be2e103a354
Shagun Madhikarmi <madhikarma@gmail.com>
2021-09-22 07:09:41 +0100

    Update build-for-testing.sh

--- build-for-testing.sh
+++ build-for-testing.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# fail if any commands fails
+set -e
+# debug log
+set -x
+
 echo "Building Xcode project for testing (Simulator)..."
 xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 8,OS=14.5" build-for-testing | xcpretty
