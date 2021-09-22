ffef19e3c3a01ed52acda5bf74ade429ae00f1fd
Shagun Madhikarmi <madhikarma@gmail.com>
2021-09-22 07:11:45 +0100

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
 
 echo "Running prebuilt tests.."
 xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 8,OS=14.5" test-without-building | xcpretty
