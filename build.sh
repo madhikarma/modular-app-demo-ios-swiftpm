c165440ffb9a5411bb3871ff792d6f5b08843d1c
Shagun Madhikarmi <madhikarma@gmail.com>
2021-09-22 07:11:30 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
 
 echo "Building Xcode project (Simulator)..."
 xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "generic/platform=iOS Simulator" clean build | xcpretty
