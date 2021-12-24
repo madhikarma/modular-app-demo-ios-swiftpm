f24aa7a3cf0a0d91dc5101d66eb5eef29a116212
Shagun Madhikarmi <madhikarma@gmail.com>
2021-12-24 07:40:10 +0000

    Adds more feature modules (Home, Search, Wishlist) and loads them into a UITabBarController

---
+++ Home/Sources/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//
+//
+//  Created by Shagun Madhikarmi on 24/12/2021.
+//
+
+import Foundation
+import UIKit
+
+final class HomeViewController: UIViewController {
+    override func loadView() {
+        view = UIView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
---
+++ Search/Sources/Search/SearchViewController.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shagun Madhikarmi on 24/12/2021.
+//
+
+import Foundation
