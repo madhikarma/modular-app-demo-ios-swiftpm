4407b5feefe669b10fb86543ff878f9046fbcd5f
Shagun Madhikarmi <madhikarma@gmail.com>
2021-12-24 15:46:33 +0000

    Restore Import code

--- StarWarsAPI/Sources/StarWarsAPI/StarWarsAPIClient.swift
+++ StarWarsAPI/Sources/StarWarsAPI/StarWarsAPIClient.swift
@@ -1,44 +1,42 @@
 //
-//  File.swift
+//  StarWarsAPIClient.swift
 //
 //
 //  Created by Shagun Madhikarmi on 24/12/2021.
 //
 
 import Foundation
-//import Networking
+import Networking
 
 public class StarWarsAPIClient {
-//    private let network = Network()
+    private let network = Network()
 
     public init() {}
 
     public func getMovies(completion: @escaping (Result<MovieResponse, Error>) -> Void) {
-//        network.sendRequest(urlString: "https://swapi.dev/api/films") { data, response, error in
-//            print(Date())
-//            print(String(describing: response))
-//
-//            var result: Result<MovieResponse, Error>
-//            if let networkError = error {
-//                result = .failure(networkError)
-//            } else if let jsonData = data {
-//                do {
-//                    let movieResponse = try JSONDecoder().decode(MovieResponse.self, from: jsonData)
-//                    print(movieResponse.results)
-//                    result = .success(movieResponse)
-//                } catch let jsonError {
-//                    result = .failure(jsonError)
-//                }
-//            } else {
-//                let error = NSError(domain: "", code: -1, userInfo: nil)
-//                result = .failure(error)
-//            }
-//
-//            DispatchQueue.main.async {
-//                completion(result)
-//            }
-//        }
-        
-        completion(.success(MovieResponse(results: [])))
+        network.sendRequest(urlString: "https://swapi.dev/api/films") { data, response, error in
+            print(Date())
+            print(String(describing: response))
+
+            var result: Result<MovieResponse, Error>
+            if let networkError = error {
+                result = .failure(networkError)
+            } else if let jsonData = data {
+                do {
+                    let movieResponse = try JSONDecoder().decode(MovieResponse.self, from: jsonData)
+                    print(movieResponse.results)
+                    result = .success(movieResponse)
+                } catch let jsonError {
+                    result = .failure(jsonError)
+                }
+            } else {
+                let error = NSError(domain: "", code: -1, userInfo: nil)
+                result = .failure(error)
+            }
+
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
     }
 }
