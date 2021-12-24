a81339801920f60f13b3fe39c480f45af61ab01f
Shagun Madhikarmi <madhikarma@gmail.com>
2021-12-24 07:55:59 +0000

    Adds network request to Home

--- Home/Sources/Home/HomeViewController.swift
+++ Home/Sources/Home/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SharedA
 import UIKit
 
 public final class HomeViewController: UIViewController {
@@ -16,7 +17,20 @@ public final class HomeViewController: UIViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .brown
-        self.title = "Home"
-        self.tabBarItem.title = "Home"
+        title = "Home"
+        tabBarItem.title = "Home"
+
+        networking.sendJSONRequest(url: URL(string: "https://swapi.dev/api/films")!) { response in
+            print(response)
+            let alertController = UIAlertController(title: "Debug", message: response.debugDescription, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+                self.delegate?.didConfirm()
+            }
+            alertController.addAction(okAction)
+
+            self.present(alertController, animated: true) {
+                self.delegate?.didPresent()
+            }
+        }
     }
 }
