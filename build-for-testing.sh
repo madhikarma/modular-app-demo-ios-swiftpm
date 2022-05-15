0d7c277cc25996451dc03c204ce56a5350ffc97b
Shagun Madhikarmi <madhikarma@gmail.com>
2022-05-15 12:06:38 +0100

    Update test device to iPhone 13 on iOS 15.4 (Since that ships in both Bitrise CI and local Xcode 13.3.1)

--- build-for-testing.sh
+++ build-for-testing.sh
@@ -6,5 +6,5 @@ set -e
 set -x
 
 echo "Building Xcode project for testing (Simulator)..."
-xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 13,OS=15.0" build-for-testing | xcpretty
+xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 13,OS=15.4" build-for-testing | xcpretty
 # xcodebuild -workspace ./ModularDemoApp.xcworkspace -scheme ModularDemoApp -clonedSourcePackagesDirPath SourcePackages -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 8,OS=14.5" build-for-testing
